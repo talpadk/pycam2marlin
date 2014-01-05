45539f59ba8fad62bad2723f6b36f31502317766
Visti Andresen <talpa@talpa.dk>
2014-01-06 00:21:44 +0100

    Added support for feed sppeds and not only using G1 moves

--- pycam2marlin.pl
+++ pycam2marlin.pl
@@ -2,19 +2,48 @@
 
 use strict;
 
+my $feedSpeed = undef;
+my $moveType = "G1";
+
+sub outputMoveLine {
+    my $line = $_[0];
+
+
+    #remove comments
+    $line =~ s/;.*//;
+
+    #remove newline
+    $line =~ s/\n//;
+
+    #move without feed speed setting
+    if ($line =~ /^\s*G1[^F]*/){
+	if (defined($feedSpeed)){
+	    $line = "$line $feedSpeed";
+	}
+    } 
+
+    print "$line\n";
+}
+
 while (<STDIN>) {
     my $line = $_;
     #move command
-    if ($line =~ /^G[10]/){
-	print $line;
+    if ($line =~ /^(G[10])/){
+	$moveType = $1;
+	outputMoveLine($line);
+    }
+    #feed speed setting
+    elsif ($line =~ /^(F\d*.?\d*)/){
+	$feedSpeed = $1;
+	print ";Feedspeed ($feedSpeed) in line: $line";
     }
     #dual XY moves
     elsif ($line =~ /^\sX\d+.?\d*\sY\d+.?\d*$/){
-	print "G1$line";
+	outputMoveLine("$moveType$line");
     }
     #single XY (Z but hasn't been seen) moves
     elsif ($line =~ /^\s[XYZ]\d+.?\d*$/){
-	print "G1$line";
+	outputMoveLine("$moveType$line");
     }
     #comment line
     elsif ($line =~ /^\s*;/){
